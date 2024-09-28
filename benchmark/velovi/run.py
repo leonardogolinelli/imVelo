6692986c2c3a7f911d133243d9ce08cca31ca539
leonardogolinelli <leogolinelli@gmail.com>
2024-09-28 15:04:36 +0200

    refactor

--- benchmark/velovi/run.py
+++ benchmark/velovi/run.py
@@ -12,12 +12,22 @@ from velovi_adapted_metrics import *
 from velovi_adapted_utils import *
 
 
-datasets = ["pancreas", "gastrulation_erythroid", "forebrain", "dentategyrus_lamanno"]
-cell_type_keys = ["clusters","celltype", "Clusters", "clusters"]
+datasets = ["forebrain", "pancreas", "gastrulation_erythroid", "dentategyrus_lamanno_P5"]
+cell_type_keys = ["Clusters", "clusters","celltype", "clusters"]
 filter_genes = [True, False]
+
 for filtered in filter_genes:
     for dataset, cell_type_key in zip(datasets, cell_type_keys):
-        adata_path = os.path.expanduser(f"~/top_adatas/mivelo_{dataset}.h5ad")
+        if dataset == "forebrain":
+            adata_path = "/mnt/data2/home/leonardo/git/multilineage_velocity/outputs_forebrain_K11_knn_rep_ve_best_key_None_0_kl_weight_1e-9_1e-08_20k/forebrain/K11/adata/adata_K11_dt_ve.h5ad"
+        elif dataset == "pancreas":
+            adata_path = "/mnt/data2/home/leonardo/git/multilineage_velocity/outputs_pancreas_K11_knn_rep_ve_best_key_None_0_kl_weight_1e-9_1e-8/pancreas/K11/adata/adata_K11_dt_ve.h5ad"
+        elif dataset == "gastrulation_erythroid":
+            adata_path = "/mnt/data2/home/leonardo/git/multilineage_velocity/outputs_gastrulation_erythroid_K11_knn_rep_ve_best_key_None_0_kl_weight_1e-9_1e-08_20k_256/gastrulation_erythroid/K11/adata/adata_K11_dt_ve.h5ad"
+        elif dataset == "dentategyrus_lamanno_P5":
+            adata_path = "/mnt/data2/home/leonardo/git/multilineage_velocity/outputs_dentategyrus_lamanno_P5_K31_knn_rep_pca_best_key_pca_unique_0_kl_weight_1e-9_1e-08_20k_12_july/dentategyrus_lamanno_P5/K31/adata/adata_K31_dt_pca.h5ad"
+
+        print(f"processing dataset: {dataset}_{filter_genes}")
         adata = sc.read_h5ad(adata_path)
         if dataset == "forebrain":
             adata.obs[cell_type_key] = [str(name) for name in adata.obs[cell_type_key]]
@@ -33,7 +43,7 @@ for filtered in filter_genes:
             print(f"new dataset name: {dataset}")
         VELOVI.setup_anndata(adata, spliced_layer="Ms", unspliced_layer="Mu")
         vae = VELOVI(adata)
-        vae.train(max_epochs=10, train_size=0.999)
+        vae.train(max_epochs=1000, train_size=0.90)
         os.makedirs(f"{dataset}/stats", exist_ok=True)
         plot_training_stats(vae, dataset)
         adata = add_velovi_outputs_to_adata(adata, vae)
@@ -50,6 +60,3 @@ for filtered in filter_genes:
         save_files(adata, vae, dataset)
         adata = extrinsic_uncertainty(adata, vae, dataset, n_samples=25)
         save_files(adata, vae, dataset)
-        adata.write_h5ad(os.path.expanduser(f"~/top_adatas/velovi_{dataset}.h5ad"))
-        VELOVI.save(vae, os.path.expanduser(f"~/top_models/velovi_{dataset}.h5ad"))
-        
